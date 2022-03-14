66d3814cfa91f3ee58d53d481105516db7f7f731
Fumattia <mattia03fuma@gmail.com>
2022-03-14 22:48:07 +0100

    aggiunta pagine di login

--- ALM/urls.py
+++ ALM/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'ALM'
 urlpatterns = [
-    path('', views.alm),
+    path('', views.home, name='home'),
+    path('area-personale', views.tempo_reale, name='tempo_reale'),
+    path('login', views.user_login, name='login'),
+    path('register', views.user_register, name='user_register'),
 ]
