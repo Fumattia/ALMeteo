77c88a7428d1b6bc9cc89d2cedc256554c29c5c8
Fumattia <mattia03fuma@gmail.com>
2022-03-29 22:23:13 +0200

    risoluzione problema login

--- ALMeteo/settings.py
+++ ALMeteo/settings.py
@@ -123,3 +123,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://almeteo.herokuapp.com','https://*.127.0.0.1']
+
